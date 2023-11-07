663249b261e298cfa7fd8205372510a1eb27945a
PamelaCalcei <pecalcei@gmail.com>
2023-11-07 15:07:19 +0100

    TP 05 -> 2

--- src/main/java/fr/diginamic/springbootdemo/Tp2Application.java
+++ src/main/java/fr/diginamic/springbootdemo/Tp2Application.java
@@ -30,9 +30,9 @@ public class Tp2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Liste des animaux : " + this.animalRepository.findAll());
-		System.out.println("Liste des personnes : " + this.personRepository.findAll());
-		System.out.println("Liste des espèces : " + this.speciesRepository.findAll());
+		System.out.println(">>>>>>>>>>Liste des animaux : " + this.animalRepository.findAll());
+		System.out.println(">>>>>>>>>>Liste des personnes : " + this.personRepository.findAll());
+		System.out.println(">>>>>>>>>>Liste des espèces : " + this.speciesRepository.findAll());
 		Animal animal1 = new Animal();
 	    animal1.setColor("Gris");
 	    animal1.setName("Léone");
@@ -40,17 +40,21 @@ public class Tp2Application implements CommandLineRunner {
 	    animal1.setSpecies(speciesRepository.findById(1).orElseThrow());
 	    animalRepository.save(animal1);
 //	    personRepository.delete(personRepository.findById(3).orElseThrow());
-	    System.out.println("Espèce de chats : " + speciesRepository.findFirstByCommonName("Chat"));
-	    System.out.println("Espèce contenant Oryctolagus : " + speciesRepository.findByLatinNameContainsIgnoreCase("oryctolagus"));
-	    System.out.println("Chercher par prénom ou nom : " + personRepository.findByLastnameOrFirstname("Mangolo", "Henri"));
-	    System.out.println("Chercher personnes avec un age supérieur ou égal : " + personRepository.findByAgeGreaterThanEqual(40));
-	    System.out.println("Renvoie tous les animaux de la Species Fournie : " + animalRepository.findBySpecies(speciesRepository.findById(1).orElseThrow()));
+	    System.out.println(">>>>>>>>>>Espèce de chats : " + speciesRepository.findFirstByCommonName("Chat"));
+	    System.out.println(">>>>>>>>>>Espèce contenant Oryctolagus : " + speciesRepository.findByLatinNameContainsIgnoreCase("oryctolagus"));
+	    System.out.println(">>>>>>>>>>Chercher par prénom ou nom : " + personRepository.findByLastnameOrFirstname("Mangolo", "Henri"));
+	    System.out.println(">>>>>>>>>>Chercher personnes avec un age supérieur ou égal : " + personRepository.findByAgeGreaterThanEqual(40));
+	    System.out.println(">>>>>>>>>>Renvoie tous les animaux de la Species Fournie : " + animalRepository.findBySpecies(speciesRepository.findById(1).orElseThrow()));
 	    List<String> colors = new ArrayList<>();
 	    colors.add("Roux");
 	    colors.add("Gris tacheté");
-	    System.out.println("Renvoie une liste de couleur : " + animalRepository.findByColorIn(colors));
-	    System.out.println("Trie les species par nom commun asc : " + speciesRepository.findByNameAsc());
-	    System.out.println("Species avec un nom commun LIKE params : " + speciesRepository.findByCommonNameLike("Chat"));
+	    System.out.println(">>>>>>>>>>Renvoie une liste de couleur : " + animalRepository.findByColorIn(colors));
+	    System.out.println(">>>>>>>>>>Trie les species par nom commun asc : " + speciesRepository.findByNameAsc());
+	    System.out.println(">>>>>>>>>>Species avec un nom commun LIKE params : " + speciesRepository.findByCommonNameLike("Chat"));
+	    System.out.println(">>>>>>>>>> Chercher personnes dont l'âge est entre age min et age max" + personRepository.findByAgeBetween(30,90));
+	    System.out.println(">>>>>>>>>> Chercher toutes les personnes qui possèdent l'animal" + personRepository.findByPersonAnimal(animalRepository.findById(4).orElseThrow()));
+	    
+	    
 	    
 	}
 }
--- src/main/java/fr/diginamic/springbootdemo/repository/PersonRepository.java
+++ src/main/java/fr/diginamic/springbootdemo/repository/PersonRepository.java
@@ -2,13 +2,20 @@ package fr.diginamic.springbootdemo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import fr.diginamic.springbootdemo.model.Animal;
 import fr.diginamic.springbootdemo.model.Person;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Integer>{
 	List<Person>findByLastnameOrFirstname(String lastname, String firstname);
 	List<Person>findByAgeGreaterThanEqual(Integer age);
+	@Query("SELECT u FROM Person u WHERE u.age BETWEEN :minAge AND :maxAge")
+	List<Person>findByAgeBetween(Integer minAge, Integer maxAge);
+	@Query("SELECT u FROM Person u inner join u.animals a WHERE a = :animal ")
+	List<Person>findByPersonAnimal(Animal animal);
+	
 }
